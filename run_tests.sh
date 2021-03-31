6a3b3f139bc037d8a4f14189ef4828903590631a
Arun Sharma <arun@sharma-home.net>
2021-03-31 11:53:24 -0700

    Use python3
    On some distros, python binary links to python2. The tests seem
    to be expecting python3.

--- run_tests.sh
+++ run_tests.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 main() {
-    python -m unittest
-    python -m examples.case_study
-    python -m examples.paper
-    python -m examples.plot_load_distribution --output "/tmp"
-    python -m examples.plot_node_loads --output "/tmp/load_nodes.pdf"
-    python -m examples.plot_workload_distribution \
+    python3 -m unittest
+    python3 -m examples.case_study
+    python3 -m examples.paper
+    python3 -m examples.plot_load_distribution --output "/tmp"
+    python3 -m examples.plot_node_loads --output "/tmp/load_nodes.pdf"
+    python3 -m examples.plot_workload_distribution \
         --output "/tmp/workload_distribution.pdf"
-    python -m examples.tutorial
+    python3 -m examples.tutorial
 }
 
 main
