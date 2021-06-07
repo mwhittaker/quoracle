ee7400a8a992b3f9f8c17822b5bab15d537e7b46
Michael Whittaker <mjwhittaker@berkeley.edu>
2021-06-07 09:03:29 -0700

    Bumped version + uploaded to pypi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="quoracle",
-    version="0.0.3",
+    version="0.0.4",
     author="Michael Whittaker",
     author_email="mwhittttaker@gmail.com",
     description=("A library for modelling, analyzing, and optimizing quorum " +
